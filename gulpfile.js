dd82ec9fde66042c0a9c9c3792106dd17f1b016f
hellokuzuki <miximixi_wang@hotmail.com>
2015-03-18 00:29:13 +1100

    add bower and gulf config file.

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+// var gulp = require('gulp');
+// var uglity = require('gulp-uglify');
+
+// gulp.task('scripts', function() {
+//   gulp.src('src/*.js')
+//   .pipe(uglity())
+//   .pipe(gulp.dest('dist'));
+// });
+
+var gulp = require('gulp');
+
+gulp.task('default',function(){
+	console.log('Hello World!');
+});
